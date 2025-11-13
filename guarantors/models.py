90001e0c678597d04cc227f38360354789eec246
Dalienst Owino <oduordalienst@gmail.com>
2025-11-13 08:55:28 +0300

    fix serializers and models

--- guarantors/models.py
+++ guarantors/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-from mwandamzeduapi.settings import MAX_GUARANTEES
 from accounts.abstracts import TimeStampedModel, UniversalIdModel, ReferenceModel
 from savings.models import SavingsAccount
 from guaranteerequests.models import GuaranteeRequest
